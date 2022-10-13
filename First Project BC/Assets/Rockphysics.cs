7098ffe51d22fe351208765a113d9e87269202a3
STUDENT Bjarne Christiansen <Bjarne.Christiansen@students.ittralee.ie>
2022-10-13 10:56:28 +0100

    Signed-off-by: STUDENT Bjarne Christiansen <Bjarne.Christiansen@students.ittralee.ie>

---
+++ First Project BC/Assets/Rockphysics.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rockphysics : MonoBehaviour
+{
+   // Rigidbody = rockRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // rockRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
